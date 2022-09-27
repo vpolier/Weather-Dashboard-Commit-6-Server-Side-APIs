0fd5c9b581ca734178db7f5c70298220522b08b9
vpolier <vpolier81@gmail.com>
2022-09-27 20:02:04 +1000

    javaScript corrections

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-$(document).ready(function () {
-    //Search button future.
+$ (document.ready(function () { 
+    
+}))
+   //Search button future.
     $("#search-button").on("click", function () {
         //Get value in input search-value.
         var searchTerm = $("#search-value").val();
         //Empty input field.
         $("#search-value").val("");
         weatherFunction(searchTerm);
-        weatherFunction(searchTerm);
+        weatherFunction(searchTerm)
 });
 
 //Search button enter key future.
@@ -63,5 +65,59 @@ function weatherFunction(searchTerm) {
 
             var title = $("<h3>").addClass("card-title").text(data.name+"("+new Date().toLocaleDateString() +")");
             var img = $("img").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-        })
-    }        
+
+            var card = $("<div>").addClass("card");
+            var cardBodyn = $("<div>").addClass("card-body");
+            var wind = $("<p>").addClass("cardtext").text("Wind Speed: " + data.wind.speed + "MPH");
+            var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+            var temp = $("<p>").addClass("card-text").text("Temperature" + data.main.temp + "K"); 
+            console.log(data)
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+
+            $.ajax({
+                type: "GET",
+                url: "https://api.openweathermap.org/data/2.5/uvi?appid=9f112416334ce37769e5c8683b218a0d&lat=" + lat + "&lon=" + lon,
+
+        }).then(function (response) {
+            console.log(response);
+
+            var uvColor;
+            var uvResponse = response.value;
+            var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
+            var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
+
+            if (uvResponse < 3) {
+                btn.addClass("btn-succes");
+            } else if (uvResponse <7) {
+                btn.addClass("btn-warning");
+            } else {
+                btn.addClass("btn-danger");
+            }
+
+            cardBodyn.append(uvIndex);
+            $("#today .card-body").append(uvIndex.append(btn));
+        });
+
+        //Merge and add to page.
+        title.append(img);
+        cardBody.append(title, temp, humid, wind);
+        card.apppend(cardBody);
+ 
+                var titleFive = $("<h3>").addClass("card-title").text(new Date (data.list[i].dt_txt).toLocaleDateString());
+                var imgFive = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+                var colFive = $("<div>").addClass("col-md-2.5");
+                var cardFive = $("<div>").addClass("card bg-primary text-white");
+                var cardBodyFive = $("<div>").addClass("card-body p-2")
+                var humidFive = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+                var tempFive = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temperature + "°F"),
+
+                //Merge toguether and out on page 
+                colFive.append(cardFive.append(cardBodyFive.append(titleFive, imgFive, humidFive)));
+                //append card to colum, boby to card, and other lements to body
+                $("#forecast . row").append(colFive);
+            } 
+        }
+    });
+}
+});
